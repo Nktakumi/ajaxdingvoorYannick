13d9a87c9cdfa317eee15381ec3ddf85278576ad
Thiago van Dieten <nktakumi@gmail.com>
2013-12-12 23:36:04 +0200

    Bestanden geupdate zodat ze werken en gecomment

--- dinges.php
+++ dinges.php
@@ -1,7 +1,12 @@
 <?php
-
-$id = $_POST['id'];
-$jsonarray = ['idNummer' => $id + 40];
+// header('Content-Type: application/json');
+//Als ik errors niet uitzet stuurt hij ook gelijk een notice dat $id niet bestaat, wtf
+error_reporting(0); 
+if(isset($_POST['id'])) {
+	$id = $_POST['id'];
+	$id += 40;
+}
+$jsonarray = ['idNummer' => $id];
 echo json_encode($jsonarray);
 
 ?>
